cbc3692dd6395ab23bb82c96df03cc88621efb2d
Ruoshi Liu <rliu@ml.cs-i.brandeis.edu>
2019-01-07 20:52:31 -0500

    splitting dataset and training

--- dataset.py
+++ dataset.py
@@ -7,6 +7,8 @@ import torch, torchvision
 from torchvision import transforms, datasets
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
+import math
+import random
 
 window_size = 50
 pad_size= window_size
@@ -22,12 +24,14 @@ def create_circular_mask(h, w, center=None, radius=None):
     dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
 
     mask = dist_from_center <= radius
-    mask = mask.astype(float)
+    mask = mask.astype(int)
     return mask
-mask = create_circular_mask(window_size, window_size)
 
-class defectDataset(Dataset):
-    def __init__(self, csv_path='/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/train.csv', img_path='/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/JPEGImages/', window_size=50, pad_size=50, mask = mask, transforms=None):
+mask = create_circular_mask(200,200)
+
+
+class defectDataset_csv(Dataset):
+    def __init__(self, csv_path='/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/train.csv', img_path='/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/JPEGImages/', window_size=50, pad_size=50, mask = create_circular_mask(200,200), transforms=None):
         """
         Args:
             csv_path (string): path to csv file
@@ -56,15 +60,18 @@ class defectDataset(Dataset):
         xmax = width * x + self.window_size/2 + self.pad_size
         ymax = height * y + self.window_size/2 + self.pad_size
         img_resized = img.crop((xmin, ymin, xmax, ymax))
+        img_resized = torchvision.transforms.functional.resize(img_resized, (200,200), interpolation=2)
+        img_masked = img_resized * mask
+        img_masked = Image.fromarray(img_masked.astype('uint8'), 'L')
 #         img_resized = img_resized * mask
 #         img_resized = img_resized*mask
 #         if self.mask is not None:
 #             img_resized[~mask] = 0
         # Transform image to tensor
         if self.transforms is not None:
-            img_resized = self.transforms(img_resized)
+            img_masked = self.transforms(img_masked)
         # Return image and the label
-        return (img_resized, single_image_label)
+        return (img_masked, single_image_label)
 
     def __len__(self):
         return len(self.data.index)
@@ -74,3 +81,110 @@ if __name__ == "__main__":
     transformations = transforms.Compose([transforms.ToTensor()])
 #     defect_from_csv = \
 #         defectDataset('../data/mnist_in_csv.csv', 28, 28, transformations)
+
+class defectDataset_df(Dataset):
+    def __init__(self, df = pd.read_csv('/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/train.csv', sep=" "), img_path='/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/JPEGImages/', window_size=50, pad_size=50, mask = create_circular_mask(200,200), transforms=None):
+        """
+        Args:
+            df: dataframes of training data
+            transform: pytorch transforms for transforms and tensor conversion
+        """
+        self.data = df
+        self.img_path = img_path
+        self.transforms = transforms
+        self.window_size = window_size
+        self.pad_size = pad_size
+        self.mask = mask
+
+    def __getitem__(self, index):
+        labels = self.data.loc[index]
+        single_image_label = int(labels['class']) # float
+        x = labels['x']
+        y = 1 - labels['y'] # origin of PIL image is top-left
+        img_index = labels['image_index']
+        img = Image.open(self.img_path + '%06.0f.jpg' % img_index)
+        img = img.convert('L')
+        img = torchvision.transforms.functional.resize(img, (300,300), interpolation=2)
+        width, height = img.size
+        img = ImageOps.expand(img, border=self.pad_size, fill=0)
+        xmin = width * x - self.window_size/2 + self.pad_size
+        ymin = height * y - self.window_size/2 + self.pad_size
+        xmax = width * x + self.window_size/2 + self.pad_size
+        ymax = height * y + self.window_size/2 + self.pad_size
+        img_resized = img.crop((xmin, ymin, xmax, ymax))
+        img_resized = torchvision.transforms.functional.resize(img_resized, (200,200), interpolation=2)
+        img_masked = img_resized * mask
+        img_masked = Image.fromarray(img_masked.astype('uint8'), 'L')
+#         img_resized = img_resized * mask
+#         img_resized = img_resized*mask
+#         if self.mask is not None:
+#             img_resized[~mask] = 0
+        # Transform image to tensor
+        if self.transforms is not None:
+            img_masked = self.transforms(img_masked)
+        # Return image and the label
+        return (img_masked, single_image_label)
+
+    def __len__(self):
+        return len(self.data.index)
+        
+
+if __name__ == "__main__":
+    transformations = transforms.Compose([transforms.ToTensor()])
+#     defect_from_csv = \
+#         defectDataset('../data/mnist_in_csv.csv', 28, 28, transformations)
+
+
+def sample_point_circular(circle_min = 0.02, circle_max = 0.07):
+    # random angle
+    alpha = 2 * math.pi * random.random()
+    # random radius
+    r = (circle_max - circle_min) * random.random() + circle_min
+    # calculating coordinates
+    x = r * math.cos(alpha)
+    y = r * math.sin(alpha)
+    return x,y
+
+def split_and_sample(df_train = pd.read_csv('/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/train.csv', sep=" ")
+                             , n_samples = 1000, non_inner_circle = 0.02, non_outer_circle = 0.07, method = 'uniform'):
+    df_pos = df_train[df_train['class'] == 0]
+    df_neg = df_train[df_train['class'] == 1]
+    df_pos_o = df_train[df_train['class'] == 2]
+    df_nuc = df_train[df_train['class'] == 3]
+    frames = [df_pos.sample(n=n_samples), df_neg.sample(n=n_samples), df_pos_o.sample(n=n_samples), df_nuc.sample(n=n_samples)]
+    df_train_samples = pd.concat(frames)
+    df_train_non = df_train_samples.sample(n=n_samples)
+    # print(df_train_non.head(10))
+    if method=='hard':
+        for index, row in df_train_non.iterrows():
+            min_dis = 0  # make sure go in for loop
+            while min_dis < non_inner_circle:
+                dx,dy = sample_point_circular(non_inner_circle, non_outer_circle)
+                new_point = [row.x+dx, row.y+dy]
+                df_image = df_train[df_train['image_index'] == row['image_index']] # retrive all points in the image
+                min_dis = 1
+                for index_im, row_im in df_image.iterrows(): # check distance from new_point to each point, if smaller than threshold than thow away
+                    dis = math.sqrt(math.pow(new_point[0]-row.x,2)+math.pow(new_point[1]-row.y,2))
+                    if dis < min_dis:
+                        min_dis = dis
+            df_train_non.at[index,'x'] = new_point[0]
+            df_train_non.at[index,'y'] = new_point[1]
+            df_train_non.at[index,'class'] = 4
+    elif method=='uniform':
+        for index, row in df_train_non.iterrows():
+            min_dis = 0
+            while min_dis < non_inner_circle:
+                new_point = [random.random(),random.random()]
+                df_image = df_train[df_train['image_index'] == row['image_index']]
+                min_dis = 1
+                for index_im, row_im in df_image.iterrows():
+                    dis = math.sqrt(math.pow(new_point[0]-row.x,2)+math.pow(new_point[1]-row.y,2))
+                    if dis < min_dis:
+                        min_dis = dis
+            df_train_non.at[index,'x'] = new_point[0]
+            df_train_non.at[index,'y'] = new_point[1]
+            df_train_non.at[index,'class'] = 4
+    df_train_samples = df_train_samples.append(df_train_non)
+    df_train_samples = df_train_samples.reset_index()
+    df_train_samples = df_train_samples.drop(['index'],axis = 1)
+    return df_train_samples
---
+++ train_hard.py
@@ -0,0 +1,199 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+from PIL import ImageOps
+import PIL
+import torch, torchvision
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+from dataset import defectDataset_df, create_circular_mask, split_and_sample
+import random
+import math
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+from torchvision import datasets, models, transforms
+import time
+import os
+from grayscale_resnet import resnet18, resnet34
+from torchvision.models.resnet import BasicBlock, conv3x3, Bottleneck
+
+window_size = 45
+pad_size = window_size
+classes = ["pos","neg","pos_o","nuc","non"]
+batch_size = 256
+
+data_transform = transforms.Compose([
+        transforms.RandomResizedCrop(200, scale=(1, 1), ratio=(1, 1)),
+        transforms.RandomRotation((-90,90)),
+        torchvision.transforms.RandomVerticalFlip(p=0.5),
+        torchvision.transforms.RandomHorizontalFlip(p=0.5),
+        torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0, hue=0),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.3019],
+                             std=[0.1909])
+    ])
+
+trainset = defectDataset_df(df = split_and_sample(method = 'hard',n_samples = 1995), window_size = window_size, transforms=data_transform)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                             batch_size=batch_size, shuffle=True,
+                                             num_workers=8)
+print("trainloader ready!")
+
+testset = defectDataset_df(df = split_and_sample(df_train = pd.read_csv('/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/test.csv', sep=" "),
+                                                      method = 'hard',n_samples = 500), window_size = window_size, transforms=data_transform)
+testloader = torch.utils.data.DataLoader(testset,
+                                             batch_size=batch_size, shuffle=True,
+                                             num_workers=8)
+print("testloader ready!")
+
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    print("GPU in use")
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+classes = ["pos","neg","pos_o","nuc","non"]
+num_of_classes = len(classes)
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training phase
+        scheduler.step()
+        model.train(True)  # Set model to training mode
+        running_loss = 0.0
+        running_corrects = 0
+
+        # Iterate over data.
+        for data in trainloader:
+            # get the inputs
+            inputs, labels = data
+#            print(inputs)
+            # wrap them in Variable
+            if use_gpu:
+#                inputs = Variable(inputs.cuda())
+#                labels = Variable(labels.cuda())
+#                inputs = torch.nn.DataParallel(inputs, device_ids=[0, 1]).cuda()
+#                labels = torch.nn.DataParallel(labels, device_ids=[0, 1]).cuda()
+                inputs, labels = inputs.to(device), labels.to(device)
+#                print(inputs)
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+
+            m = nn.LogSoftmax(dim=0)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(m(outputs), labels)
+            # forward
+#            outputs = model(inputs)
+#            _, preds = torch.max(outputs.data, 1)
+#            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            # statistics
+            iter_loss = loss.item()
+            correct = torch.sum(preds == labels.data).item()
+            batch_accuracy = correct / batch_size
+            running_loss += loss.item()
+            running_corrects_tensor = torch.sum(preds == labels.data)
+            running_corrects += running_corrects_tensor.item()        
+            epoch_loss = running_loss / len(trainset)
+            epoch_acc = running_corrects / len(trainset)
+            
+            print('{} Loss: {:.4f} Acc: {:.4f} batch_loss: {:.4f} correct: {:d} batch_accuracy: {:.4f}'.format(
+                "train", epoch_loss, epoch_acc, iter_loss, correct, batch_accuracy))
+#         correct = 0
+#         total = 0
+#         with torch.no_grad():
+#             for data in testloader:
+#                 images, labels = data
+#                 inputs, labels = inputs.to(device), labels.to(device)
+#                 outputs = model(images)
+#                 _, predicted = torch.max(outputs.data, 1)
+#                 total += labels.size(0)
+#                 correct += (predicted == labels).sum().item()
+# #                print('processed: %d' % total)
+# #                print('correct: %d' % correct)
+#         print('Accuracy of the network on the test images: %.5f %%' % (
+#             100 * correct / total))
+    
+        correct = 0
+        total = 0
+        class_correct = list(0. for i in range(5))
+        class_total = list(0. for i in range(5))
+        with torch.no_grad():
+            for data in testloader:
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model_ft(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                c = (predicted == labels).squeeze()
+                for i in range(batch_size):
+                    if len(labels) == batch_size:
+                        label = labels[i]
+                        class_correct[label] += c[i].item()
+                        class_total[label] += 1
+        #         print(predicted)
+        #         print(labels)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        #       print('processed: %d' % total)
+        #       print('correct: %d' % correct)
+            print('Accuracy of the network on the test images: %.5f %%' % (100 * correct / total))
+            for i in range(5):
+                print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
+
+        
+        
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+# transfer learning resnet18
+model_ft = resnet34()
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, num_of_classes)
+
+if use_gpu:
+    model_ft = torch.nn.DataParallel(model_ft)
+    model_ft.to(device)
+
+criterion = nn.NLLLoss()
+#criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+# optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.01,  momentum=0.9)
+optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.00025)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=100, gamma=0.1)
+
+# train model
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=500)
+torch.save(model_ft, output_path)
+
+
+

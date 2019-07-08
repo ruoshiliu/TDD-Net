48c3a22f3796c7d7124ea6f08b22c92546db31bb
ruoshiliu <ruoshiliu@brandeis.edu>
2019-07-08 12:10:44 -0400

    cleaned train_uniform.py

--- train_uniform.py
+++ train_uniform.py
@@ -24,19 +24,21 @@ pad_size = window_size
 classes = ["pos","neg","pos_o","nuc","non"]
 output_path = '/home/rliu/defect_classifier/models/python/res34_600epo_uniform_01-10-18.model'
 batch_size = 256
-non_pos_ratio = 4
+non_pos_ratio = 1
+train_num = 1995
+test_num = 500
+mode = 'tiny' # or "tiny"
+method = 'uniform'
 
 data_transform = transforms.Compose([
         transforms.RandomResizedCrop(200, scale=(1, 1), ratio=(1, 1)),
         transforms.RandomRotation((-90,90)),
         torchvision.transforms.RandomVerticalFlip(p=0.5),
         torchvision.transforms.RandomHorizontalFlip(p=0.5),
-#         torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0, hue=0),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.3019],
                              std=[0.1909])
     ])
-
 
 use_gpu = torch.cuda.is_available()
 if use_gpu:
@@ -47,7 +49,34 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 classes = ["pos","neg","pos_o","nuc","non"]
 num_of_classes = len(classes)
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+# transfer learning resnet34
+if mode == "full":
+    model = resnet34()
+elif mode == "tiny":
+    model = resnet18()
+
+# change output channel for last fully connected layer according to number of classes
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, num_of_classes) 
+
+if use_gpu:
+    model = torch.nn.DataParallel(model)
+    model.to(device)
+
+# adjust weights of negative samples during training
+weights = [1.0, 1.0, 1.0, 1.0, 1.0/non_pos_ratio]  
+class_weights = torch.FloatTensor(weights).to(device)
+
+criterion = nn.NLLLoss(weight = class_weights)
+# optimizer = optim.SGD(model.parameters(), lr=0.01,  momentum=0.9)
+optimizer = optim.Adam(model.parameters(), lr=0.00025)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.5)
+
+
+def train_model(model, criterion, optimizer, scheduler, transform, num_epochs=500, method = 'uniform'):
+    print(model)
     since = time.time()
 
     best_model_wts = model.state_dict()
@@ -63,14 +92,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         running_loss = 0.0
         running_corrects = 0
 
-        trainset = defectDataset_df(df = split_and_sample(method = 'uniform',n_samples = 1995, non_pos_ratio = non_pos_ratio), window_size = window_size, transforms=data_transform)
+        trainset = defectDataset_df(df = split_and_sample(df_labels = pd.read_csv('/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/train.csv', sep=" "),
+                                                          method = method, n_samples = train_num, non_pos_ratio = non_pos_ratio), window_size = window_size, transforms=transform)
         trainloader = torch.utils.data.DataLoader(trainset,
                                              batch_size=batch_size, shuffle=True,
                                              num_workers=16, drop_last=True)
         print("trainloader ready!")
 
         testset = defectDataset_df(df = split_and_sample(df_labels = pd.read_csv('/home/rliu/yolo2/v2_pytorch_yolo2/data/an_data/VOCdevkit/VOC2007/csv_labels/test.csv', sep=" "),
-                                                              method = 'uniform',n_samples = 500), window_size = window_size, transforms=data_transform)
+                                                              method = method, n_samples = test_num), window_size = window_size, transforms=transform)
         testloader = torch.utils.data.DataLoader(testset,
                                                      batch_size=batch_size, shuffle=True,
                                                      num_workers=16)
@@ -79,30 +109,18 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         for data in trainloader:
             # get the inputs
             inputs, labels = data
-#            print(inputs)
             # wrap them in Variable
             if use_gpu:
-#                inputs = Variable(inputs.cuda())
-#                labels = Variable(labels.cuda())
-#                inputs = torch.nn.DataParallel(inputs, device_ids=[0, 1]).cuda()
-#                labels = torch.nn.DataParallel(labels, device_ids=[0, 1]).cuda()
                 inputs, labels = inputs.to(device), labels.to(device)
-#                print(inputs)
             else:
                 inputs, labels = Variable(inputs), Variable(labels)
 
             # zero the parameter gradients
             optimizer.zero_grad()
-
-
             m = nn.LogSoftmax(dim=0)
             outputs = model(inputs)
             _, preds = torch.max(outputs.data, 1)
             loss = criterion(m(outputs), labels)
-            # forward
-#            outputs = model(inputs)
-#            _, preds = torch.max(outputs.data, 1)
-#            loss = criterion(outputs, labels)
 
             loss.backward()
             optimizer.step()
@@ -119,20 +137,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             
             print('{} Loss: {:.4f} Acc: {:.4f} batch_loss: {:.4f} correct: {:d} batch_accuracy: {:.4f}'.format(
                 "train", epoch_loss, epoch_acc, iter_loss, correct, batch_accuracy))
-#         correct = 0
-#         total = 0
-#         with torch.no_grad():
-#             for data in testloader:
-#                 images, labels = data
-#                 inputs, labels = inputs.to(device), labels.to(device)
-#                 outputs = model(images)
-#                 _, predicted = torch.max(outputs.data, 1)
-#                 total += labels.size(0)
-#                 correct += (predicted == labels).sum().item()
-# #                print('processed: %d' % total)
-# #                print('correct: %d' % correct)
-#         print('Accuracy of the network on the test images: %.5f %%' % (
-#             100 * correct / total))
     
         correct = 0
         total = 0
@@ -142,7 +146,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             for data in testloader:
                 inputs, labels = data
                 inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model_ft(inputs)
+                outputs = model(inputs)
                 _, predicted = torch.max(outputs.data, 1)
                 c = (predicted == labels).squeeze()
                 for i in range(batch_size):
@@ -150,18 +154,11 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                         label = labels[i]
                         class_correct[label] += c[i].item()
                         class_total[label] += 1
-        #         print(predicted)
-        #         print(labels)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-        #       print('processed: %d' % total)
-        #       print('correct: %d' % correct)
             print('Accuracy of the network on the test images: %.5f %%' % (100 * correct / total))
             for i in range(5):
                 print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
-
-        
-        
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -172,34 +169,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     model.load_state_dict(best_model_wts)
     return model
 
-
-# transfer learning resnet18
-model_ft = resnet34()
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, num_of_classes)
-
-if use_gpu:
-    model_ft = torch.nn.DataParallel(model_ft)
-    model_ft.to(device)
-
-weights = [1.0, 1.0, 1.0, 1.0, 1.0/non_pos_ratio]
-class_weights = torch.FloatTensor(weights).to(device)
-criterion = nn.NLLLoss(weight = class_weights)
-#criterion = nn.CrossEntropyLoss()
-
-# Observe that all parameters are being optimized
-# optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.01,  momentum=0.9)
-optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.00025)
-
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=100, gamma=0.5)
-
 # train model
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=800)
-torch.save(model_ft, output_path)
-
-
-
-
-
+model = train_model(model, criterion, optimizer, exp_lr_scheduler, data_transform, num_epochs=800, method = method)
+torch.save(model.module, output_path)
